ab7e29e1b774c77e80b1252fe0171a861defed94
Carmen Salas <clivingrooms@aol.com>
2018-04-16 21:01:25 +0000

    search

--- js/script.js
+++ js/script.js
@@ -4,7 +4,40 @@
 
 $(document).ready(function(){
   
-  
+  function appendImageToBody(srcURL) {
+    // this function appends an <img> to the body with the
+    // URL provided in the parameters
+    $(".gallery").append("<img src=" + srcURL + ">");
+}
+
+// DO NOT MODIFY ABOVE THIS LINE. READ THE COMMENTS BELOW TO COMPLETE THE FUNCTIONS.
+
+// write a function that will return a url for the giphy API with
+// the searchTerm provided in the parameters
+function giphyURLWithSearchTerm(searchTerm) {
+return "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC" ; 
+
+}
+
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+        // modify this url to take the searchTerm into account (hint: which function gives you the api url with a searchTerm as a parameter?)
+        url: giphyURLWithSearchTerm(searchTerm) ,
+        method: "GET",
+        success: function(response) {
+            var url = response.data[0].images.original.url;
+            // call the appendImageToBody function to add the image to the page
+        appendImageToBody(url);            
+      },
+    }); 
+}
+
+// Fix the click handler so that callGiphyAPIWithSearchTerm is called with the user's input
+$("button").click(function (){
+    var searchTerm = $("input").val();
+    callGiphyAPIWithSearchTerm(searchTerm);
+});
   
   
 });
